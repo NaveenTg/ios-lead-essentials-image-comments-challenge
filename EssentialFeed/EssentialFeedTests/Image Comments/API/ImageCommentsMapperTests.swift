2163159514de8bb9cdf020dc320b71a576c12b82
Naveen <naveentg.ntg@gmail.com>
2021-07-23 15:53:52 +0530

    Validate ImageCommentsMapper throws error on non 2xx response

--- EssentialFeed/EssentialFeedTests/Image Comments/API/ImageCommentsMapperTests.swift
+++ EssentialFeed/EssentialFeedTests/Image Comments/API/ImageCommentsMapperTests.swift
@@ -8,7 +8,7 @@ import EssentialFeed
 class ImageCommentsMapperTests: XCTestCase {
 	func test_map_throwsErrorOnNon200HTTPResponse() throws {
 		let json = makeItemsJSON([])
-		let samples = [199, 201, 300, 400, 500]
+		let samples = [199, 130, 300, 400, 500]
 
 		try samples.forEach { code in
 			XCTAssertThrowsError(
