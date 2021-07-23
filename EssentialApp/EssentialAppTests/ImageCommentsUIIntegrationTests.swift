d349cdf115f7b49e03bc51ddbd15be19d7c6d061
Naveen <naveentg.ntg@gmail.com>
2021-07-23 23:32:25 +0530

    Prove any running request is cancelled in Image Comments when View is deinited

--- EssentialApp/EssentialAppTests/ImageCommentsUIIntegrationTests.swift
+++ EssentialApp/EssentialAppTests/ImageCommentsUIIntegrationTests.swift
@@ -128,6 +128,23 @@ class ImageCommentsUIIntegrationTests: XCTestCase {
 		XCTAssertEqual(sut.errorMessage, nil)
 	}
 
+	func test_View_deinit_cancelsRunningRequest() {
+		var cancelCallCount = 0
+		var sut: ListViewController?
+		autoreleasepool {
+			sut = ImageCommentsUIComposer.imageCommentsComposedWith(commentsLoader: {
+				PassthroughSubject<[ImageComment], Error>()
+					.handleEvents(receiveCancel: {
+						cancelCallCount += 1
+					}).eraseToAnyPublisher()
+			})
+			sut?.loadViewIfNeeded()
+		}
+		XCTAssertEqual(cancelCallCount, 0)
+		sut = nil
+		XCTAssertEqual(cancelCallCount, 1)
+	}
+
 	// MARK: - Helpers
 
 	private func makeSUT(
