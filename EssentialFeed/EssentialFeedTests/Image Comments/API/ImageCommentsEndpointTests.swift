755b19bff86abfb1395abd317488d691740e3acb
Naveen <naveentg.ntg@gmail.com>
2021-07-30 20:53:29 +0530

    Hardcoded image.id to avoid extra logic in end point tests

--- EssentialFeed/EssentialFeedTests/Image Comments/API/ImageCommentsEndpointTests.swift
+++ EssentialFeed/EssentialFeedTests/Image Comments/API/ImageCommentsEndpointTests.swift
@@ -8,7 +8,8 @@ import EssentialFeed
 class ImageCommentsEndpointTests: XCTestCase {
 	func test_imageComments_endpointURL() {
 		let baseURL = URL(string: "http://base-url.com")!
-		let image = FeedImage(id: UUID(), description: "A Image Description", location: "World", url: anyURL())
+		let uuidString = UUID(uuidString: "132e4765-e89b-12d3-a456-421466174000")!
+		let image = FeedImage(id: uuidString, description: "A Image Description", location: "World", url: anyURL())
 		let receivedURL = ImageCommentsEndpoint.get(image).url(baseURL: baseURL)
 		let expectedURL = URL(string: "http://base-url.com/v1/image/\(image.id)/comments")!
 
