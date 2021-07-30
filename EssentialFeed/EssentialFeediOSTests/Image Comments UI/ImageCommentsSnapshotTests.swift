4414e5deca22b67baaeb2a525c32ce7add1f4348
Naveen <naveentg.ntg@gmail.com>
2021-07-30 21:14:29 +0530

    Testing image comment UI snapshot test with long username and very long message

--- EssentialFeed/EssentialFeediOSTests/Image Comments UI/ImageCommentsSnapshotTests.swift
+++ EssentialFeed/EssentialFeediOSTests/Image Comments UI/ImageCommentsSnapshotTests.swift
@@ -42,9 +42,9 @@ class ImageCommentsSnapshotTests: XCTestCase {
 				                             date: "999 years ago",
 				                             username: "First Guy")),
 			ImageCommentCellController(
-				model: ImageCommentViewModel(message: "East Side Gallery\n is in Berlin Germany",
+				model: ImageCommentViewModel(message: "The East Side Gallery is an open-air gallery in Berlin. It consists of a series of murals painted directly on a 1,316 m long remnant of the Berlin Wall, located near the centre of Berlin, on Mühlenstraße in Friedrichshain-Kreuzberg. The gallery has official status as a Denkmal, or heritage-protected landmark.",
 				                             date: "9 days ago",
-				                             username: "Second Guy")),
+				                             username: "Second Guy in  East Side Gallery\nMemorial in Berlin, Germany")),
 			ImageCommentCellController(
 				model: ImageCommentViewModel(message: "A Nice Place",
 				                             date: "9 minutes ago",
