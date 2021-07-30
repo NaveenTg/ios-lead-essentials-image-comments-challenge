39192ac1374271c21633d92e14aaa186d2579111
Naveen <naveentg.ntg@gmail.com>
2021-07-30 20:26:14 +0530

    Shortening static function call isOk in ImageCommentMapper

--- EssentialFeed/EssentialFeed/Image Comments/API/ImageCommentsMapper.swift
+++ EssentialFeed/EssentialFeed/Image Comments/API/ImageCommentsMapper.swift
@@ -35,7 +35,7 @@ public final class ImageCommentsMapper {
 	public static func map(_ data: Data, from response: HTTPURLResponse) throws -> [ImageComment] {
 		let decoder = JSONDecoder()
 		decoder.dateDecodingStrategy = .iso8601
-		guard ImageCommentsMapper.isOK(response), let root = try? decoder.decode(Root.self, from: data) else {
+		guard isOK(response), let root = try? decoder.decode(Root.self, from: data) else {
 			throw Error.invalidData
 		}
 
